193f5f5635f4f924ce3d46076379334ca3de044b
michivo <michivo@gmail.com>
2023-02-28 21:31:39 +0100

    start with canvas for map

---
+++ src/routes/Map.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		// resize the canvas to fill browser window dynamically
+		window.addEventListener('resize', resizeCanvas, false);
+
+		function resizeCanvas() {
+			canvas.width = window.innerWidth;
+			canvas.height = window.innerHeight;
+		}
+
+		resizeCanvas();
+
+		const canvas = document.getElementById('map') as HTMLCanvasElement;
+
+		if (canvas) {
+			const context = canvas.getContext('2d');
+			const mapImage = new Image();
+			mapImage.src = './images/plan.png';
+			mapImage.onload = function () {
+				console.log('drawing image');
+				context?.drawImage(mapImage, 0, 0);
+			};
+		}
+	});
+</script>
+
+<canvas id="map" width="1900" height="1000" />
